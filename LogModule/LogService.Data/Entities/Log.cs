968637755152b4e857f3022bd5918eaabf61633b
HakanAk <hakanak54@gmail.com>
2024-01-26 12:46:05 +0300

    Add project files.

---
+++ LogModule/LogService.Data/Entities/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogService.Data.Entities
+{
+    public class Log
+    {
+        public string Id { get; set; } = null!;
+        public string ServiceName { get; set; } = null!;
+        public string LogType { get; set; } = null!;
+        public string LogException { get; set; } = null!;
+        public DateTime LogDate { get; set; }
+    }
+}
